1d3356ea0557cab556fba4587f5cea7c793b25b0
saloni-jain-code <sjain102015@gmail.com>
2024-03-23 13:06:32 -0400

    phase 2 complete, rendered notes from notes.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,22 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
+function createNote(note){
+    return (
+        <Note
+            title={note.title}
+            content={note.content}
+        />
+    );
+}
 function App() {
+    console.log(notes)
     return (
         <div>
             <Header />
-            <Note />
-            <Note />
-            <Note />
+            {notes.map(createNote)}
             <Footer />
         </div>
     );
